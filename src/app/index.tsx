c40a1be7dc4331f6f9b016ca50b1323809f18d7a
Alperen Sirli <alperensirli99@hotmail.com>
2026-06-01 23:36:35 +0200

    fetching pokemons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,25 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function Index() {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
+        const data = await res.json();
+        setPokemon(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPokemon();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Pokedex</Text>
     </View>
   );
 }
