c8a6f7cdbf31b3180cc32691e5319be680121ac6
Alperen Sirli <alperensirli99@hotmail.com>
2026-06-02 13:26:43 +0200

    color variable based on pokemon type & display

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -15,6 +15,27 @@ interface PokemonType {
   };
 }
 
+const colorsByType: Record<string, string> = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
+
 export default function Index() {
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
 
@@ -47,11 +68,22 @@ export default function Index() {
   }, []);
 
   return (
-    <ScrollView>
+    <ScrollView
+      contentContainerStyle={{
+        gap: 16,
+        padding: 16,
+      }}
+    >
       {pokemon.map((p) => (
-        <View key={p.name}>
+        <View
+          key={p.name}
+          style={{
+            // @ts-ignore
+            backgroundColor: colorsByType[p.types[0].type.name],
+          }}
+        >
           <Text style={styles.name}>{p.name}</Text>
-          <Text>{p.types[0].type.name}</Text>
+          <Text style={styles.type}>{p.types[0].type.name}</Text>
           <View
             style={{
               flexDirection: "row",
@@ -75,7 +107,12 @@ export default function Index() {
 // styling variables
 const styles = StyleSheet.create({
   name: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  type: {
     fontSize: 20,
     fontWeight: "bold",
+    color: "gray",
   },
 });
